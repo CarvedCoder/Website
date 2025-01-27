4ae0c7ebe7fa878484b7a06b18ac9c12b9d90700
CarvedCoder <shreyansbehera84@gmail.com>
2025-01-27 17:18:30 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home({products}) {
       <Head>
         <title>E-commerce website</title>
       </Head>
-
+        <Header/>
         <main className ="max-w-screen-2xl mx-auto">
           <Banner />
           
