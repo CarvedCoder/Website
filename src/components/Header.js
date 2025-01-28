9f4f6fd49caf26ada012a7ec373cb625900e7995
CarvedCoder <shreyansbehera84@gmail.com>
2025-01-28 21:38:21 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header() {
             {/* Upper nav*/}
             <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
                 <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
-                    <   Image
+                    <   Image ="https://cdn.discordapp.com/avatars/686903620490493970/a_85aa7a603b194e218c62d8140a147157.gif?size=80"
                         width={150}
                         height={40}
                         objectFit="contain"
